754b0014b024247fc2bb41491b5927673406fc4e
Mustafa Abdelkawy <32801595+Abdelkawy16@users.noreply.github.com>
2022-06-24 20:31:12 +0200

    Add project files.

---
+++ JQueryDatatable/Controllers/CustomersController.cs
@@ -0,0 +1,44 @@
+﻿using JQueryDatatable.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+
+namespace JQueryDatatable.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult GetCustomers()
+        {
+            var pageSize = int.Parse(Request.Form["length"]);
+            var skip = int.Parse(Request.Form["start"]);
+            var searchValue = Request.Form["search[value]"].ToString().ToLower();
+            var sortColumn = Request.Form[string.Concat("columns[", Request.Form["order[0][column]"], "][name]")];
+            var sortColumnDirection = Request.Form["order[0][dir]"];
+
+            var customers = _context.Customers.Where(c=> 
+            c.FirstName.ToLower().Contains(searchValue)
+            || c.FirstName.ToLower().Contains(searchValue)
+            || c.LastName.ToLower().Contains(searchValue)
+            || c.Contact.ToLower().Contains(searchValue)
+            || c.Email.ToLower().Contains(searchValue)
+            );
+
+            if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+                customers = customers.OrderBy(sortColumn + " " + sortColumnDirection);
+
+            var data = customers.Skip(skip).Take(pageSize).ToList();
+            var totalRecords = customers.Count();
+            var jsonData = new {recordsFiltered=totalRecords, totalRecords, data};
+            return Ok(jsonData);
+        }
+    }
+}
---
+++ JQueryDatatable/wwwroot/js/CustomersDatartable.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(() => {
+    $('#Customers').dataTable({
+        "serverSide": true,
+        "filter": true,
+        "ajax": {
+            "url": "/api/customers",
+            "type": "POSt",
+            "dataType":"json"
+        },
+        "columnDefs": [{
+            "targets": [0],
+            "visible": false,
+            "searchable": false
+        }],
+        "columns": [
+            { "data": "id", "name": "id", "autowidth": true },
+            { "data": "firstName", "name": "firstName", "autowidth": true },
+            { "data": "lastName", "name": "lastName", "autowidth": true },
+            { "data": "contact", "name": "contact", "autowidth": true },
+            { "data": "email", "name": "email", "autowidth": true },
+            { "data": "dateOfBirth", "name": "dateOfBirth", "autowidth": true },
+            {
+                "render": (data, type, row) =>
+                { return `<a href="#" class="btn btn-danger" onclick=DeleteCustomer(${row.id})>Delete</a>` },
+                "orderable": false
+            },
+        ]
+    })
+})
